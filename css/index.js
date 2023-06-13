24bf6a7aa449059e381ba746eb75679676b110b9
Nakul Yadav <nakulyadav1710@gmail.com>
2023-06-13 21:08:17 +0530

    spoyipy

--- css/index.js
+++ css/index.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+for (var i=0; i<6; i++){
+    var star = ""+""+""+""
+    for(var j=0; j<i+1; j++){
+       var star += "*" 
+    }
+    console.log(star) 
+}
+for (var i=5; i>-1; i--){
+    var star = ""+""+""+""
+    for(var j=0; j<i+1; j++){
+       var star += "*" 
+    }
+    console.log(star) 
+}
+
+

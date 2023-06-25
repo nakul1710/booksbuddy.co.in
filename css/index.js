37881207ba4a25dbe923dbcdefe39020a46400c5
Nakul Yadav <nakulyadav1710@gmail.com>
2023-06-25 21:03:24 +0530

    nkulji

--- css/index.js
+++ css/index.js
@@ -1,16 +1,3 @@
-var num = 9;
-for (var i=1; i<=num; i++){
-    var star = ""
-    var space = ""
-    var lowerIndex = 1 + space/2
-    var higherIndex = num - space/2
-    for(var j=1; j<=i; j++){
-        if(j > lowerIndex && j < higherIndex) {
-            star += "*" 
-        }else{
-            star += " "
-        }
-    }
-    console.log(star)
-}
+var letter = 'abc';
+letter.match(/a/)
 

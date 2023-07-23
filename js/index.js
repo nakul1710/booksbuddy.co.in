b090aed3db56fd9cda465a79c3a92fca566072f3
Nakul Yadav <nakulyadav1710@gmail.com>
2023-07-23 21:04:31 +0530

    javascript

--- js/index.js
+++ js/index.js
@@ -113,3 +113,11 @@ for (let i = 1; i <= rows; i++) {
 
   console.log(pattern);
 }
+const rows = 5;
+for (let i = rows; i >= 1; i--) {
+  let pattern = "";
+  for (let j = 1; j <= i; j++) {
+    pattern += "*";
+  }
+  console.log(pattern);
+}

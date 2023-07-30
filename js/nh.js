43791d8b0ace432822adc59aa6107e7ddaa71be1
Nakul Yadav <nakulyadav1710@gmail.com>
2023-07-30 10:44:33 +0530

    netflix

--- js/nh.js
+++ js/nh.js
@@ -1,25 +1,23 @@
-function matrixAddition(matrixA,matrixB){
-    const numRows = matrix.length;
-    const numCols = matrix[0].length;
-    const resultMatrix = [];
 
-    for(let i=0; i <numRows; i++ ){
-        resultMatrix[i] = []
-    for(let j=0; j <numCols; j++){
-        resultMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
-    }
-  }
-  return resultMatrix;
+
+/*const number= 12345;
+let sum = 0;
+let temp = number;
+while(temp>0){
+    sum += temp%10;
+    Math.floor(temp/10);
 }
+console.log("the sum of the number:",sum);*/
+const primeNumber = 17;
+let isPrime = true;
+if(primeNumber < 2){
+  isPrime = false;
+}else {
+    for(i=2; i <= Math.sqrt(primeNumber); i++ ){
+        if(primeNumber%i === 0){
+            isPrime = true;
+            break;
+        }
 
-const matrixA = [
-    [2,4],
-    [3,4],
-];
-const matrixB = [
-    [3,9],
-    [1,6],
-];
-
-const matrixSum = matrixAddition(matrixA,matrixB);
-console.log("matrix Addition result:",matrixSum);
+    }
+}

c4c8010c9acf24186a052cc4c20d56d34946514e
Nakul Yadav <nakulyadav1710@gmail.com>
2023-08-01 08:53:19 +0530

    work

--- js/nh.js
+++ js/nh.js
@@ -1,15 +1,4 @@
-function matrixSubtraction(matrixA, matrixB){
-    const numrows = matrixA.length;
-    const numcols = matrixA[0].length;
-    const resultantMatrix = [];
-    for(let i=0; i < numrows; i++){
-        resultantMatrix[i] = []
-    for(let j=0; j < numcols; j++){
-        resultantMatrix[i][j] = matrixA[i][j]- matrixB[i][j];
-    }
-    }
-    return resultantMatrix;
-}
+
 const matrixA = [
     [2,5],
     [3,5],
@@ -20,3 +9,18 @@ const matrixB = [
 ];
 const subtract = matrixSubtraction(matrixA,matrixB);
 console.log("substract:",subtract);
+function matrixSubtraction(matrixA,matrixB){
+    const numrows = matrixA.length;
+    const numcols = matrixA[0].length;
+    const resultantMatrix = [];
+    for(let i=0; i < numrows; i++){
+        resultantMatrix[i] = [];
+    for(let j=0; j < numcols; j++){
+        resultantMatrix[i][j] = matrixA[i][j]+matrixB[i][j];
+
+    }
+    }
+    return resultantMatrix;
+}
+const substract = matrixSubtraction(matrixA,matrixB)
+console.log("the subtraction is",substract);

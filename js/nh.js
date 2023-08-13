953fd03eec0fa598bd947ccb33851eeada0ab624
Nakul Yadav <nakulyadav1710@gmail.com>
2023-08-13 15:51:40 +0530

    assignment-html

--- js/nh.js
+++ js/nh.js
@@ -1,28 +1,5 @@
+import { PI, add } from './module/math.js';
 
-const matrixA = [
-    [2,5],
-    [3,5],
-];
-const matrixB = [
-    [2,4]
-    [1,3]
-];
-const subtract = matrixSubtraction(matrixA,matrixB);
-console.log("substract:",subtract);
-function matrixSubtraction(matrixA,matrixB){
-    const numrows = matrixA.length;
-    const numcols = matrixA[0].length;
-    const resultantMatrix = [];
-    for(let i=0; i < numrows; i++){
-        resultantMatrix[i] = [];
-    for(let j=0; j < numcols; j++){
-        resultantMatrix[i][j] = matrixA[i][j]+matrixB[i][j];
+console.log(PI); // Output: 3.14
 
-    }
-    }
-    return resultantMatrix;
-}
-const substract = matrixSubtraction(matrixA,matrixB)
-console.log("the subtraction is",substract);
-
-
+console.log(add(2, 3));

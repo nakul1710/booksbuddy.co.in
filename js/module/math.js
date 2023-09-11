7812e71fe4c9b3fee37effa7044615b219e9928e
Nakul Yadav <nakulyadav1710@gmail.com>
2023-09-11 21:14:51 +0530

    bootstrap

--- js/module/math.js
+++
@@ -1,5 +0,0 @@
-export const PI = 3.14;
-
-export function add(a, b) {
-  return a + b;
-}
--- js/nh.js
+++
@@ -1,5 +0,0 @@
-import { PI, add } from './module/math.js';
-
-console.log(PI); // Output: 3.14
-
-console.log(add(2, 3));

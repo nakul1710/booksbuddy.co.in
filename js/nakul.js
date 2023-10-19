9725e1c4100125438ccfeaaf77af547d9e5e55b3
Nakul Yadav <nakulyadav1710@gmail.com>
2023-10-19 21:44:14 +0530

    nakul.dev

--- js/nakul.js
+++ js/nakul.js
@@ -4,15 +4,29 @@ for (let i = 0; i<number.length; i++){
     sum += number[i];
 }
 console.log("the sum of the array is:",sum);
-const arrays = [10, 20, 30, 40, 50];
-const searchValue = 30;
-let found = true;
 
-for (let i = 0; i < arrays.length; i++) {
-  if (array[i] === searchValue) {
-    found = true;
+
+const elements = [1,2,3,4,5];
+for(let i=0; i<elements.length;  i++  ){
+    console.log(elements[i]);
+}
+
+const numbers = [4,9,2,7,5];
+let maxNumber = number[0];
+for (let i=0; i<numbers.length; i++){
+    if(numbers[i] > maxNumber){
+        maxNumber = numbers[i];
+    }
+}
+console.log("The maximum number is:",maxNumber);
+const numbersArrays = [10, 20, 25, 30, 40];
+const removeValue = 25;
+
+for (let i = 0; i < numbersArrays.length; i++) {
+  if (numbersArrays[i] === removeValue) {
+    numbersArrays.splice(i, 1);
     break;
   }
 }
+console.log("The updated array is:", numbersArrays);
 
-console.log("The element is", found ? "found" : "not found", "in the array.");

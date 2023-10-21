6821d4883806621a5f6362fb905af8bf7a4c0f46
Nakul Yadav <nakulyadav1710@gmail.com>
2023-10-21 14:30:38 +0530

    javascript- average and palindrome

--- js/nakul.js
+++ js/nakul.js
@@ -85,3 +85,13 @@ for(let i=0; i<num.length; i++){
 }
 const average = su/num.length;
 console.log("The average is",average);
+let a = 5;
+let b = 10;
+let temp;
+console.log("Before swapping: a =", a, "b =", b);
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("After swapping: a =", a, "b =", b);
